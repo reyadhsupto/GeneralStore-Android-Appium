ce7441fbe5a43602e75b402403242620530df9f7
Reyad Hassan <hassan.reyad01@gmail.com>
2025-05-30 12:35:58 +0600

    test/ Add test to check if user can fillup form

---
+++ src/main/java/Pages/FormPage.java
@@ -0,0 +1,68 @@
+package Pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.AppiumDriver;
+
+public class FormPage extends BasePage{
+	
+	private final By yourName = AppiumBy.id("com.androidsample.generalstore:id/nameField");
+	private final By radioMale = AppiumBy.id("com.androidsample.generalstore:id/radioMale");
+	private final By letShop = AppiumBy.id("com.androidsample.generalstore:id/btnLetsShop");
+	private final By dropdown = AppiumBy.id("android:id/text1");
+	private String country = "";
+	
+	private final By nextPage = AppiumBy.id("com.androidsample.generalstore:id/toolbar_title");
+	public final By nameError = AppiumBy.xpath("//android.widget.Toast[1]");
+	
+	private By getDynamicCountryLocator(String countryName) {
+	    return By.xpath(String.format("//android.widget.TextView[@text='%s']", countryName));
+	}
+	
+	
+	public FormPage(AppiumDriver driver, WebDriverWait wait) {
+        this.driver = driver;
+        this.wait = wait;
+        this.actionChain = new Actions(driver);
+    }
+	
+	
+	public void chooseCountry(String countryName) {
+		ClickElement(dropdown);
+		country = countryName;
+		ScrollToText(country);
+		ClickElement(getDynamicCountryLocator(countryName));
+		
+	}
+	
+	public void fillName(String name) {
+		SendKeys(yourName, name);
+	}
+	
+	public void checkGender() {
+		ClickElement(radioMale);
+	}
+	
+	public void letsShop() {
+		ClickElement(letShop);
+	}
+	
+	public String checkNextPage() {
+		return GetElement(nextPage).getDomProperty("text");
+	}
+	
+	public boolean checkNameError() {
+		return GetElement(nameError).getText().trim().contains("Please enter your name");
+	}
+	
+	public boolean isCountrySelected(String name) {
+		return GetElement(dropdown).getText().trim().equals(name);
+	}
+	
+	
+
+}
---
+++ src/test/java/Tests/TestFormPage.java
@@ -0,0 +1,35 @@
+package Tests;
+import static org.testng.Assert.assertTrue;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import Pages.FormPage;
+
+public class TestFormPage extends baseTest{
+	
+	public String country = "Bangladesh";
+	public String name = "Reyad Hassan";
+	public String nextPageText = "Products";
+	public FormPage form;
+	
+	@BeforeMethod
+	public void initPageObjects() {
+        form = new FormPage(driver , wait);
+    }
+	
+	@Test(priority = 1, description = "TC_001: Verify that user is able to fillup the form", enabled = true)
+    public void testFormFillup() {
+        form.chooseCountry(country);
+        Assert.assertTrue(form.isCountrySelected(country), "Country selection failed");
+        form.letsShop();
+        Assert.assertTrue(form.checkNameError(), "Error message not displayed if name is not provided");
+        form.fillName(name);
+        form.checkGender();
+        form.letsShop();
+//        String actualText = form.checkNextPage();
+//        Assert.assertEquals(actualText, nextPageText, "User no redirected to products page upon form fillup!");
+    }
+
+}
